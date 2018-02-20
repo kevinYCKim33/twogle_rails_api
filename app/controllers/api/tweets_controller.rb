2bdf6e2087434e20be1e57e2ac2586a74f7ceb52
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-19 17:13:42 -0800

    tweets getting rendered in back end, token hidden in .env

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -1,7 +1,32 @@
 class Api::TweetsController < ApplicationController
   def index
-    render json: Tweet.all
   end
 
+  def create
+  end
 
-end
+  def load_tweets
+    search = params[:search]
+    begin
+      @resp = Faraday.get 'https://api.twitter.com/1.1/search/tweets.json' do |req|
+          req.params['q'] = search
+          req.params['result_type'] = 'mixed' #mixed, recent, popular
+          req.params['count'] = 30 #doesn't work with popular
+          req.params['tweet_mode'] = 'extended'
+          req.headers['Content-Type'] = 'application/json'
+          req.headers['Authorization'] = "Bearer " + ENV['BEARER_TOKEN']
+          # req.options.timeout = 0
+        end
+        body = JSON.parse(@resp.body)
+        if @resp.success?
+          @tweets = body["statuses"]
+        else
+          @error = body["meta"]["errorDetail"]
+        end
+
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
+    render json: @tweets
+  end
+end #end class
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	namespace :api do
 		resources :tweets, except: [:new, :edit]
+		post '/tweets/load_tweets', to: 'tweets#load_tweets'
 	end
 end
