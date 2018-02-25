25457310c5d14bae905c30f083e7eaa38337dd30
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-25 02:19:18 -0800

    clear all history working

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,13 +1,16 @@
 class Api::SearchesController < ApplicationController
   def index
+    # binding.pry
+    render json: Search.all
   end
 
   def create
-    binding.pry
+    search = Search.find_or_create_by(keywords: params["search"], search_url: CGI::escape(params[:search]))
+    render json: search
   end
 
-  def delete
-
+  def destroy_all
+    Search.destroy_all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 	namespace :api do
 		resources :tweets, except: [:new, :edit]
-		resources :searches, only: [:index, :create, :delete ]
-		# post '/searches', to: 'searches#create'
+		resources :searches, only: [:index, :create, :destroy]
+		# delete '/searches/destroy_all', to 'searches#destroy_all'
+		get '/searches/clear', to: 'searches#destroy_all'
 		post '/tweets/load_tweets', to: 'tweets#load_tweets'
 		post '/headlines/load_headlines', to: 'headlines#load_headlines'
 	end
