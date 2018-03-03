6844b34a6d9f8e03eb606cbbbae02cb18dbb093b
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-03-02 23:08:14 -0800

    allowing twogle-herokuapp.com as allowable cors site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000' #['localhost:3000', 'twogle.herokuapp.com'] if you want to add heroku later
+    origins ['localhost:3000', 'twogle-herokuapp.com'] if you want to add heroku later
 
     resource '*',
       headers: :any,
